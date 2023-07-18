1d15668a27705edb9d82cfcfeb4da05345da6a20
Jeffrey Cheung <jeffreycheung_@live.com>
2023-07-18 14:39:28 -0700

    implement payment page conditional rendering

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import CheckoutWizard from "../components/CheckoutWizard";
 
-export default function page() {
+export default function Payment() {
+  const cookieStore = cookies();
+  const cartToken = cookieStore.get("cartItems");
+  const cartItems = cartToken && JSON.parse(cartToken.value);
+  const shippingToken = cookieStore.get("shippingInfo");
+  const shippingInfo = shippingToken && JSON.parse(shippingToken.value);
+
   return (
-    <div>page</div>
+    <div className="min-h-80vh w-11/12 max-w-[1350px] mx-auto">
+      <h1 className="text-center font-semibold my-8 text-4xl">Payment</h1>
+      <CheckoutWizard activeStep={2} />
+      {
+        !shippingInfo?.firstName
+          || !shippingInfo?.lastName
+          || !shippingInfo?.email
+          || !shippingInfo?.address
+          || !shippingInfo?.city
+          || !shippingInfo?.province
+          || !shippingInfo?.postalCode
+          ? cartItems === undefined || cartItems.length < 1
+            ? redirect('/cart')
+            : redirect('/checkout')
+          : (
+            <></>
+          )}
+    </div>
   )
 }
