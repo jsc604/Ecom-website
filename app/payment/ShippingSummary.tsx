a678a70e6d4f58772e82c180c083366fa9f86532
Jeffrey Cheung <jeffreycheung_@live.com>
2023-07-21 17:12:58 -0700

    style shipping info

---
+++ app/payment/ShippingSummary.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { Typography, Card } from "@mui/material"
+import { useContext } from "react";
+import { Store } from "@/utils/StoreProvider";
+import { Home, LocalShipping, LocationOn, MyLocation, Person } from "@mui/icons-material";
+
+export default function ShippingSummary() {
+  const { shippingInfo } = useContext(Store);
+
+  return (
+    <Card  className="space-y-2" sx={{ padding: 2 }}>
+      <Typography sx={{ fontSize: 30, fontWeight: 600, textAlign: 'center' }}>Shipping Address</Typography>
+      <Typography><Person /> {shippingInfo?.firstName} {shippingInfo?.lastName}</Typography>
+      <Typography><Home /> {shippingInfo?.address}</Typography>
+      <Typography><LocationOn /> {shippingInfo?.city}, {shippingInfo?.province}, Canada</Typography>
+      <Typography><MyLocation /> {shippingInfo?.postalCode}</Typography>
+      <Typography><LocalShipping /> {shippingInfo?.shippingOption}</Typography>
+    </Card>
+  )
+}
